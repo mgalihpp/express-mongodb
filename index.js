fcab930d4398b060cce8bc93b325932f9b52f9ea
mgalihpp <muhammadgalih451@gmail.com>
2023-08-15 20:30:29 +0700

    add cors

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connectDB = require("./connectMongo");
+const listModel = require("./models/list");
+require("dotenv").config();
 
-const app = express()
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:3000", // Allow requests from this origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Specify the allowed methods
+  })
+);
 
-require('dotenv').config()
-
-app.use(express.json())
-
-const connectDB = require('./connectMongo')
-
-connectDB()
-
-const listModel = require('./models/list')
-
-const PORT = process.env.PORT
+connectDB();
 
 app.get("/List", async (req, res) => {
     try {
         const list = await listModel.find({});
         return res.status(200).json(list);
-    } catch (error){
-        res.status(404).json({ error : "error to find list"});
+    } catch (error) {
+        res.status(404).json({ error: "error to find list" });
     }
-})
+});
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT)
-})
+    console.log("Server is running on port " + PORT);
+});
